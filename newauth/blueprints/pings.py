17eb1376190bf720af2cb05919f15e22c81c7315
adrien-f <adrien.fillon@gmail.com>
2014-09-03 22:54:24 +0200

    Logic tweak to pinger user list calculation

--- newauth/blueprints/pings.py
+++ newauth/blueprints/pings.py
@@ -93,8 +93,11 @@ class PingsView(FlaskView):
                     else:
                         ping_users = contacts_users
                 else:
-                    flash("Empty results. Nobody would receive this ping.", 'danger')
-                    return redirect(url_for('PingsView:new'))
+                    if groups_users:
+                        ping_users = groups_users
+                    else:
+                        flash("Empty results. Nobody would receive this ping.", 'danger')
+                        return redirect(url_for('PingsView:new'))
 
             ping_message = ping_form.message.data + '\n' + '== broadcast at {d} (UTC/EVE) from {u} to {r} =='
             recipients = ', '.join(scopes + [group.name for group in groups] + [contact.name for contact in contacts])
