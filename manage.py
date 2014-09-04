e9d68bc3f6327f3497cb79fc98e9bb5d0eacb606
adrien-f <adrien.fillon@gmail.com>
2014-09-04 20:19:36 +0200

    Add ldap_import manage command

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 import logging
 import sys
+from ldap3 import SEARCH_SCOPE_WHOLE_SUBTREE
 from flask.ext.assets import ManageAssets
 from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager, Server
 from newauth.app import create_app
-from newauth.models import db, AuthContact, User, Group, GroupMembership
+from newauth.models import db, AuthContact, User, Group, GroupMembership, APIKey
 from newauth.models.enums import GroupType
+from newauth.plugins.sync.ldap import ldap_sync, LDAPUser
 
 app = create_app()
 
@@ -35,6 +37,67 @@ def make_admin(user):
         db.session.add(g)
         db.session.commit()
 
+
+@manager.command
+def make_ping(user):
+    with app.app_context():
+        u = User.query.filter_by(user_id=user).first()
+        if not u:
+            raise Exception('Could not find user {}.'.format(user))
+        g = Group.query.filter_by(name=app.config['PING_GROUP']).first()
+        if not g:
+            g = Group(name=app.config['PING_GROUP'], description='The ping group.', type=GroupType.hidden.value)
+        membership = GroupMembership(user=u, is_admin=True, can_ping=True)
+        g.members.append(membership)
+        db.session.add(g)
+        db.session.commit()
+
+
+@manager.command
+def import_from_ldap():
+    """
+    This command will read all entries in a ldap directory and import its users,
+    running the necessary API calls.
+    If the provided API Key is not working, the user will not be imported.
+    :return:
+    """
+    with app.app_context():
+        ldap_users = []
+        with ldap_sync.connection as c:
+            result = c.search(app.config['SYNC_LDAP_MEMBERDN'], '(uid=*)', SEARCH_SCOPE_WHOLE_SUBTREE, attributes=['*'])
+            if result:
+                for user in c.response:
+                    # Is user already in Db ?
+                    password = user['attributes']['userPassword'][0]
+                    ldap_user = LDAPUser.from_ldap(user)
+                    user_db = User.query.filter_by(user_id=ldap_user.uid).first()
+                    if user_db:
+                        app.logger.debug('{} is already registered.'.format(ldap_user.uid))
+                        continue
+                    user_model = User(
+                        user_id=ldap_user.uid,
+                        email=ldap_user.email,
+                        password=password,
+                        name=ldap_user.characterName
+                    )
+                    api_key = APIKey(
+                        key_id=ldap_user.keyID,
+                        vcode=ldap_user.vCode
+                    )
+                    for character in api_key.get_characters():
+                        user_model.characters.append(character)
+                    for character in user_model.characters:
+                        if character.name == ldap_user.characterName:
+                            user_model.main_character_id = character.id
+                            break
+                    api_key.update_api_key()
+                    user_model.api_keys.append(api_key)
+                    db.session.add(user_model)
+                    db.session.commit()
+                    user_model.update_keys()
+                    user_model.update_status()
+                    db.session.add(user_model)
+                    db.session.commit()
 
 
 if __name__ == '__main__':
