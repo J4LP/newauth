e4963cdb9dd45bcc52883a1ac5708d4999d29330
adrien-f <adrien.fillon@gmail.com>
2014-09-09 14:59:03 +0200

    Limit session to an IP and redirect to login if it does not match

--- newauth/app.py
+++ newauth/app.py
@@ -1,6 +1,6 @@
 import os
 from blinker import Namespace
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, session, request, flash
 from flask_wtf import CsrfProtect
 from werkzeug.utils import import_string
 
@@ -67,5 +67,16 @@ def create_app():
                 'dashboard_hooks': app.dashboard_hooks
             }
         }
+
+    @app.before_request
+    def check_session():
+        if session.get('ip') != request.remote_addr:
+            session.clear()
+            session['ip'] = request.remote_addr
+            flash('Session expired, please login.')
+            return redirect(url_for('AccountView:login'))
+
+
+
 
     return app
