2b06d1af05d0171de1710124a7176a0ff925fa35
adrien-f <adrien.fillon@gmail.com>
2014-10-26 18:45:32 +0100

    Add a update_users command to manage.py

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import sys
 from ldap3 import SEARCH_SCOPE_WHOLE_SUBTREE
@@ -55,6 +56,34 @@ def make_ping(user):
         g.members.append(membership)
         db.session.add(g)
         db.session.commit()
+
+
+@manager.command
+def update_users(user_id=None):
+    with app.app_context():
+        if user_id:
+            user = User.query.filter_by(user_id=user_id).first()
+            if not user:
+                current_app.logger.warn('Could not find user #{}'.format(user_id))
+                return
+            user.update_keys()
+            user.update_status()
+            db.session.add(user)
+            db.session.commit()
+        else:
+            users = User.query.filter_by().all()
+            print('Found {} users.'.format(len(users)))
+            count = len(users)
+            i = 0
+            for user in users:
+                i += 1
+                print('Updating {}'.format(user.user_id))
+                print('{} %'.format((i / count) * 100))
+                user.update_keys()
+                user.update_status()
+                db.session.add(user)
+                db.session.commit()
+                time.sleep(1)
 
 
 if __name__ == '__main__':
