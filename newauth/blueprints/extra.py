5ca480027b22ef9e775d19100cab9e02ec0cf88d
adrien-f <adrien.fillon@gmail.com>
2014-10-30 19:07:39 +0100

    #3: Differentiate between alliance and corporation on the standing page and ordering by name

--- newauth/blueprints/extra.py
+++ newauth/blueprints/extra.py
@@ -2,6 +2,7 @@ from flask import flash, request, current_app, render_template, redirect, url_fo
 from flask.ext.classy import FlaskView, route
 from flask.ext.login import current_user, login_required
 from newauth.models import AuthContact
+from newauth.models.enums import AuthContactType
 from newauth.tasks import update_contacts
 
 
@@ -15,5 +16,15 @@ class ExtraView(FlaskView):
             return redirect(url_for('AdminView:standings', updated=True))
         if request.args.get('updated') and current_user.is_admin():
             flash('Contacts will be updated shortly.', 'success')
-        contacts = AuthContact.query.all()
+        contacts = {
+            '-10': {'corporations': [], 'alliances': []},
+            '-5': {'corporations': [], 'alliances': []},
+            '5': {'corporations': [], 'alliances': []},
+            '10': {'corporations': [], 'alliances': []},
+        }
+        for contact in AuthContact.query.filter(AuthContact.standing != 0).all():
+            if contact.get_type() == AuthContactType.alliance:
+                contacts[str(contact.standing)]['alliances'].append(contact)
+            else:
+                contacts[str(contact.standing)]['corporations'].append(contact)
         return render_template('extra/standings.html', contacts=contacts)
